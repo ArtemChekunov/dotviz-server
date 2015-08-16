29d18243055f34fbff6e21feda8f00e27e38add4
Artem Chekunov <scorp.dev.null@gmail.com>
2015-08-16 14:55:41 +0300

    fixed README and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,13 @@ MAINTAINER Chekunov Artem <scorp.dev.null@gmail.com>
 
 
 ADD . /go/src/github.com/sc0rp1us/dotviz-server
+ADD entrypoint.sh /tmp/entrypoint.sh
 WORKDIR /go/src/github.com/sc0rp1us/dotviz-server
 
 RUN apt-get update && apt-get install -y graphviz
 RUN go get github.com/sc0rp1us/dotviz-server
 RUN go install github.com/sc0rp1us/dotviz-server
 
-ENTRYPOINT /go/bin/dotviz-server
+ENTRYPOINT /tmp/entrypoint.sh
 
 EXPOSE 1234
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,14 +4,14 @@ echo "${DOTVIZ_PORT}" "${DOTVIZ_HOST}"
 
 
 if [ -n "${DOTVIZ_PORT}" ] &&  [ -n "${DOTVIZ_HOST}" ] ; then
-  dotviz-server -host ${DOTVIZ_HOST} -port ${DOTVIZ_PORT}
+  /go/bin/dotviz-server -host ${DOTVIZ_HOST} -port ${DOTVIZ_PORT}
 
 elif [ -n "${DOTVIZ_HOST}" ] ; then
-  dotviz-server -host ${DOTVIZ_HOST}
+  /go/bin/dotviz-server -host ${DOTVIZ_HOST}
 
 elif [ -n "${DOTVIZ_PORT}" ] ; then
-  dotviz-server -port ${DOTVIZ_PORT}
+  /go/bin/dotviz-server -port ${DOTVIZ_PORT}
 
 else
-  dotviz-server
+  /go/bin/dotviz-server
 fi
